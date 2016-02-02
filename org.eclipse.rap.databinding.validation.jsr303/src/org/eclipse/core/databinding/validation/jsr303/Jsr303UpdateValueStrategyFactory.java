5cfa66fe6c02bbcce0dc2949b3dbbfbe1b5c51d3
Martin Ferlan <martinferlan@atron.de>
2016-02-02 12:34:34 +0100

    cross constraint validtor  (e.g. FieldMatchValidator)
    Change-Id: I768aeea7a47104bc8a7dd63c22ca2f315d3184ae

--- org.eclipse.rap.databinding.validation.jsr303/src/org/eclipse/core/databinding/validation/jsr303/Jsr303UpdateValueStrategyFactory.java
+++ org.eclipse.rap.databinding.validation.jsr303/src/org/eclipse/core/databinding/validation/jsr303/Jsr303UpdateValueStrategyFactory.java
@@ -18,41 +18,6 @@ public class Jsr303UpdateValueStrategyFactory {
      *            the class type of the Pojo to validate (ex: Person.class).
      * @param propertyName
      *            the property name to validate (ex: "email" if there is Person#getEmail()).
-     * @return
-     */
-    public static UpdateValueStrategy create(final Class beanType, final String propertyName) {
-        return Jsr303UpdateValueStrategyFactory.create( beanType, propertyName, false );
-    }
-
-    /**
-     * Create {@link UpdateValueStrategy} configured with {@link Jsr303PropertyValidator} to
-     * validate the given propertyName of the given class beanType.
-     *
-     * @param beanType
-     *            the class type of the Pojo to validate (ex: Person.class).
-     * @param propertyName
-     *            the property name to validate (ex: "email" if there is Person#getEmail()).
-     * @param multiStatus
-     *            true if validator must return a {@link MultiStatus} with the whole errors and
-     *            false otherwise.
-     * @return
-     */
-    public static UpdateValueStrategy create(final Class beanType, final String propertyName,
-                                             boolean multiStatus) {
-        UpdateValueStrategy strategy = new UpdateValueStrategy();
-        strategy.setAfterConvertValidator(
-                        new Jsr303PropertyValidator( beanType, propertyName, multiStatus ) );
-        return strategy;
-    }
-
-    /**
-     * Create {@link UpdateValueStrategy} configured with {@link Jsr303PropertyValidator} to
-     * validate the given propertyName of the given class beanType.
-     *
-     * @param beanType
-     *            the class type of the Pojo to validate (ex: Person.class).
-     * @param propertyName
-     *            the property name to validate (ex: "email" if there is Person#getEmail()).
      * @param multiStatus
      *            true if validator must return a {@link MultiStatus} with the whole errors and
      *            false otherwise.
@@ -66,10 +31,31 @@ public class Jsr303UpdateValueStrategyFactory {
      */
     public static UpdateValueStrategy create(final Class beanType, final String propertyName,
                                              boolean multiStatus, boolean provideDefaults,
-                                             int updatePolicy) {
+                                             int updatePolicy, Class< ? >... validationGroups) {
         UpdateValueStrategy strategy = new UpdateValueStrategy( provideDefaults, updatePolicy );
-        strategy.setAfterConvertValidator(
-                        new Jsr303PropertyValidator( beanType, propertyName, multiStatus ) );
+        strategy.setAfterConvertValidator( new Jsr303PropertyValidator( beanType, propertyName,
+                        multiStatus, validationGroups ) );
+        return strategy;
+    }
+
+    /**
+     * Create {@link UpdateValueStrategy} configured with {@link Jsr303PropertyValidator} to
+     * validate the given propertyName of the given class beanType.
+     *
+     * @param beanType
+     *            the class type of the Pojo to validate (ex: Person.class).
+     * @param propertyName
+     *            the property name to validate (ex: "email" if there is Person#getEmail()).
+     * @param multiStatus
+     *            true if validator must return a {@link MultiStatus} with the whole errors and
+     *            false otherwise.
+     * @return
+     */
+    public static UpdateValueStrategy create(final Class beanType, final String propertyName,
+                                             boolean multiStatus, Class< ? >... validationGroups) {
+        UpdateValueStrategy strategy = new UpdateValueStrategy();
+        strategy.setAfterConvertValidator( new Jsr303PropertyValidator( beanType, propertyName,
+                        multiStatus, validationGroups ) );
         return strategy;
     }
 
@@ -90,9 +76,26 @@ public class Jsr303UpdateValueStrategyFactory {
      * @return
      */
     public static UpdateValueStrategy create(final Class beanType, final String propertyName,
-                                             boolean provideDefaults, int updatePolicy) {
+                                             boolean provideDefaults, int updatePolicy,
+                                             Class< ? >... validationGroups) {
         return Jsr303UpdateValueStrategyFactory.create( beanType, propertyName, false,
-                        provideDefaults, updatePolicy );
+                        provideDefaults, updatePolicy, validationGroups );
+    }
+
+    /**
+     * Create {@link UpdateValueStrategy} configured with {@link Jsr303PropertyValidator} to
+     * validate the given propertyName of the given class beanType.
+     *
+     * @param beanType
+     *            the class type of the Pojo to validate (ex: Person.class).
+     * @param propertyName
+     *            the property name to validate (ex: "email" if there is Person#getEmail()).
+     * @return
+     */
+    public static UpdateValueStrategy create(final Class beanType, final String propertyName,
+                                             Class< ? >... validationGroups) {
+        return Jsr303UpdateValueStrategyFactory.create( beanType, propertyName, false,
+                        validationGroups );
     }
 
 }
